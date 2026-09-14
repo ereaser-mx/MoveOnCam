626ddb0bbd73a0ddebcb8458d67a9f09da1dab8a
ereaser-mx <ereasermx@gmail.com>
2026-09-14 14:40:35 -0700

    Creacion de los modulos

---
+++ python/main.py
@@ -0,0 +1,8 @@
+\* Author:  Brigadistas del servicio social 2026-2027
+\*          Alumnos UAS de Lic. Informatica
+\* 
+\* Version: 0.1
+\* Date: 14-09-2026
+\*
+\* P R O G R A M A  D E  A S I S T E N C I A  P A R A 
+\*      R E H A B I L I T A C I O N  M O T R I Z 
---
+++ python/vision.py
@@ -0,0 +1,1 @@
+\* Modulo de vision por computadora
